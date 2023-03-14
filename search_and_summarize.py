48938397127b2f9876e1047199c9d3ae84c477e2
Ken Takahashi <30580351+ktakahashig@users.noreply.github.com>
2023-03-14 10:41:51 -0500

    Update search_and_summarize.py

--- search_and_summarize.py
+++ search_and_summarize.py
@@ -12,12 +12,15 @@ interpretaciones = pd.read_csv(interpretaciones_resumidas)
 interpretaciones['embeddings']=interpretaciones.embeddings.apply(
     lambda s: list(ast.literal_eval(s)))
 
-st.title('O4C')
+st.title('ClimaCiencia')
+
+st.heading('Reporte de interpretaciones')
+st.write('Ingrese abajo su consulta para buscar entre las interpretaciones disponibles en el Observatorio del Conocimiento Científico sobre Cambio Climático O4C y generar un reporte automatizado con inteligencia artificial.')
 
 query = st.text_input("Busque en el O4C:" ) 
 
-with st.spinner('Pensando ...'):
-    report=o4c.search_n_summarize(interpretaciones,query)
+#with st.spinner('Pensando ...'):
+report=o4c.search_n_summarize(interpretaciones,query)
 
 st.success("Reporte basado en interpretaciones más relevantes a: "+query+"\n\n")
 st.write(report)
