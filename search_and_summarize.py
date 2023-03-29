2bf4320e1b6ba1f9c7811b12de19c2723af874ae
Ken Takahashi <30580351+ktakahashig@users.noreply.github.com>
2023-03-28 22:11:21 -0500

    Update search_and_summarize.py

--- search_and_summarize.py
+++ search_and_summarize.py
@@ -7,6 +7,43 @@ import ast
 import time
 import re
 
+#### For azure
+from gunicorn.app.base import BaseApplication
+
+class StreamlitApp(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+def app(environ, start_response):
+    if environ.get('PATH_INFO', '').startswith('/'):
+        st_app = streamlit_app
+    else:
+        st_app = other_app
+    return st_app(environ, start_response)
+
+def run_streamlit_app(app, port=5000):
+    options = {
+        'bind': f'0.0.0.0:{port}',
+        'workers': 1,
+    }
+    StreamlitApp(app, options).run()
+
+if __name__ == '__main__':
+    run_streamlit_app(app)
+####
+
+
 openai.api_key  = st.secrets["OPENAI_API_KEY"]
 
 interpretaciones_resumidas='interpretaciones_resumidas.csv'
